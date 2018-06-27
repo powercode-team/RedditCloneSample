75a477a3910e1f8a118cea28c1aaaf97ac170996
meugen <evgeniy.m@powercode.us>
2018-06-27 15:36:02 +0300

    Remove duplicate code in algorithms

--- app/src/main/java/example/powercode/us/redditclonesample/common/Algorithms.java
+++ app/src/main/java/example/powercode/us/redditclonesample/common/Algorithms.java
@@ -3,6 +3,7 @@ package example.powercode.us.redditclonesample.common;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 
+import java.util.Arrays;
 import java.util.Iterator;
 import java.util.List;
 
@@ -30,11 +31,7 @@ public final class Algorithms {
     public static <T> void processElementIf(@NonNull T[] sequence,
                                             @NonNull Predicate<? super T> condition,
                                             @NonNull Consumer<? super T> onTrue) {
-        for (T el : sequence) {
-            if (condition.test(el)) {
-                onTrue.accept(el);
-            }
-        }
+        processElementIf(Arrays.asList(sequence), condition, onTrue);
     }
 
     public static <T> void processElementIf(@NonNull Iterable<? extends T> sequence,
@@ -52,13 +49,7 @@ public final class Algorithms {
     public static <T> void processElementIf(@NonNull T[] sequence,
                                             @NonNull Predicate<? super T> condition,
                                             @NonNull Consumer<? super T> onTrue, @NonNull Consumer<? super T> onFalse) {
-        for (T el : sequence) {
-            if (condition.test(el)) {
-                onTrue.accept(el);
-            } else {
-                onFalse.accept(el);
-            }
-        }
+        processElementIf(Arrays.asList(sequence), condition, onTrue, onFalse);
     }
 
     @Nullable
@@ -73,13 +64,7 @@ public final class Algorithms {
     }
 
     public static <T> int findIndex(@NonNull T[] array, @NonNull Predicate<? super T> filter) {
-        for (int i = 0; i < array.length; i++) {
-            if (filter.test(array[i])) {
-                return i;
-            }
-        }
-
-        return -1;
+        return findIndex(Arrays.asList(array), filter);
     }
 
     public static <T> int findIndex(@NonNull List<T> array, @NonNull Predicate<? super T> filter) {
@@ -107,16 +92,13 @@ public final class Algorithms {
 
     @SuppressWarnings("UnusedReturnValue")
     public static <T> boolean removeElementOnce(@NonNull Iterable<? extends T> sequence, @NonNull Predicate<? super T> filter) {
-        boolean removed = false;
         final Iterator<? extends T> each = sequence.iterator();
         while (each.hasNext()) {
             if (filter.test(each.next())) {
                 each.remove();
-                removed = true;
-
-                break;
+                return true;
             }
         }
-        return removed;
+        return false;
     }
 }
